99c217fd8746358aba57c64f9ce014ff1824eca2
u1pe <1419013@ed.tus.ac.jp>
2020-12-08 23:24:55 +0900

    third commit

--- vect3.hpp
+++ vect3.hpp
@@ -24,5 +24,24 @@ vect3 operator+(const vect3& a, const vect3& b) {
 vect3 operator-(const vect3& a, const vect3& b) {
   return vect3(a.x - b.x, a.y - b.y, a.z - b.z);
 }
+vect3 operator*(const vect3& a, const double b) {
+  return vect3(b * a.x, b * a.y, b * a.z);
+}
+vect3 operator*(const double b, const vect3& a) {
+  return vect3(b * a.x, b * a.y, b * a.z);
+}
+double dot(const vect3& a, const vect3& b) {
+  return (a.x * b.x) + (a.y * b.y) + (a.z * b.z);
+}
+double vect3norm1(const vect3& a) {
+  return std::abs(a.x) + std::abs(a.y) + std::abs(a.z);
+}
+double vect3norm2(const vect3& a) {
+  return (a.x * a.x) + (a.y * a.y) + (a.z * a.z);
+}
+double vect3norminfty(const vect3& a, const vect3& b) {
+  return std::max(std::abs(a.x), std::abs(a.y), std::abs(a.z));
+}
+double length(const vect3& a) { return std::sqrt(vect3norm2(a)); }
 
 #endif
